750d2a537ba781682981d920bbd5f50f14249370
Macbook <macbook@MacBook-Air-de-Macbook.local>
2023-12-22 10:44:47 +0100

    small correction

--- src/Model/ISubjectManager.php
+++ src/Model/ISubjectManager.php
@@ -5,7 +5,6 @@ namespace App\Model;
 interface ISubjectManager
 {
     public function insert(array $subject, int $user): string;
-    
+
     public function update(array $subject, int $id): bool;
 }
-
