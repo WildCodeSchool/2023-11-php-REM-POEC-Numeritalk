df5a31059ab97d059fe551f9ffc117646c56d89d
kiing78 <kev97250@hotmail.fr>
2024-01-03 10:35:14 +0100

    resolve conflict database

--- database.sql
+++ database.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 12 déc. 2023 à 14:34
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,47 +18,207 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `simple-mvc`
+-- Base de données : `numeritalk`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `item`
+-- Structure de la table `categorie`
 --
 
-CREATE TABLE `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `categorie` (
+  `id` int(11) NOT NULL,
+  `cat_name` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Contenu de la table `item`
+-- Déchargement des données de la table `categorie`
 --
 
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
+INSERT INTO `categorie` (`id`, `cat_name`) VALUES
+(1, 'php'),
+(2, 'java');
+
+-- --------------------------------------------------------
 
 --
--- Index pour les tables exportées
+-- Structure de la table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL,
+  `mes_contenu` varchar(70) NOT NULL,
+  `sujet` int(11) NOT NULL,
+  `utilisateur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `message`
+--
+
+INSERT INTO `message` (`id`, `mes_contenu`, `sujet`, `utilisateur`) VALUES
+(1, 'salut a vous jeune pandawan', 1, 1),
+(2, 'bonjour', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `rol_name` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`id`, `rol_name`) VALUES
+(1, 'utilisateur');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sujet`
+--
+
+CREATE TABLE `sujet` (
+  `id` int(11) NOT NULL,
+  `suj_name` varchar(40) NOT NULL,
+  `categorie` int(11) NOT NULL,
+  `utilisateur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `sujet`
+--
+
+INSERT INTO `sujet` (`id`, `suj_name`, `categorie`, `utilisateur`) VALUES
+(1, 'comment creer une appli java', 2, 1),
+(2, 'php est tres bien', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `id` int(11) NOT NULL,
+  `uti_name` varchar(50) DEFAULT NULL,
+  `uti_password` varchar(50) DEFAULT NULL,
+  `role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `uti_name`, `uti_password`, `role`) VALUES
+(1, 'jaquie', 'test', 1),
+(2, 'michel', 'test2', 1);
+
+--
+-- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `item`
+-- Index pour la table `categorie`
 --
-ALTER TABLE `item`
+ALTER TABLE `categorie`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- Index pour la table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sujet` (`sujet`),
+  ADD KEY `utilisateur` (`utilisateur`);
+
+--
+-- Index pour la table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `sujet`
+--
+ALTER TABLE `sujet`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categorie` (`categorie`),
+  ADD KEY `utilisateur` (`utilisateur`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role` (`role`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT pour la table `item`
+-- AUTO_INCREMENT pour la table `categorie`
 --
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `categorie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `sujet`
+--
+ALTER TABLE `sujet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`sujet`) REFERENCES `sujet` (`id`),
+  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`utilisateur`) REFERENCES `utilisateur` (`id`);
+
+--
+-- Contraintes pour la table `sujet`
+--
+ALTER TABLE `sujet`
+  ADD CONSTRAINT `sujet_ibfk_1` FOREIGN KEY (`categorie`) REFERENCES `categorie` (`id`),
+  ADD CONSTRAINT `sujet_ibfk_2` FOREIGN KEY (`utilisateur`) REFERENCES `utilisateur` (`id`);
+
+--
+-- Contraintes pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`role`) REFERENCES `role` (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
